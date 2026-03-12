ccad73ba24af53038a61d7a0103aabf775f51c4e
Kevin Gyori <gyorijonatan@gmail.com>
2026-03-12 23:25:05 +0000

    Stop opening debug control window on launch

--- AppHarpoon/App/AppDelegate.swift
+++ AppHarpoon/App/AppDelegate.swift
@@ -3,13 +3,10 @@ import AppKit
 @MainActor
 final class AppDelegate: NSObject, NSApplicationDelegate {
     private var statusItemController: StatusItemController?
-    private var controlPanelWindowController: ControlPanelWindowController?
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         NSApp.setActivationPolicy(.accessory)
         AppModel.shared.start()
         statusItemController = StatusItemController(appModel: AppModel.shared)
-        controlPanelWindowController = ControlPanelWindowController(appModel: AppModel.shared)
-        controlPanelWindowController?.show()
     }
 }
