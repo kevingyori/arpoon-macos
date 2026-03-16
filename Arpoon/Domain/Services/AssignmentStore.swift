5841441f37b0a0e4c3b9a7eb6b37e429f4891351
Kevin Gyori <gyorijonatan@gmail.com>
2026-03-16 23:55:03 +0000

    Make assignment persistence async and actor-safe

--- Arpoon/Domain/Services/AssignmentStore.swift
+++ Arpoon/Domain/Services/AssignmentStore.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+@MainActor
 protocol AssignmentStore {
     func loadAssignments() async throws -> [SlotAssignment]
     func saveAssignments(_ assignments: [SlotAssignment]) async throws
